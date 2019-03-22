095f4b0effc70fd0c5ddceac0146ac63bfcd0e5c
akio-outori <jhallyburton@aquent.com>
2019-03-22 17:34:43 -0400

    add basic makefile

--- algorithms1/Percolation.java
+++ algorithms1/Percolation.java
@@ -1,4 +1,6 @@
-import java.util.Arrays;
+import edu.princeton.cs.algs4.StdRandom;
+import edu.princeton.cs.algs4.StdStats;
+import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
 public class Percolation {
 
