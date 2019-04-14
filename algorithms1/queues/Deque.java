403cde0bdd6678fe10bd69f18111f16fc746421c
Jeff Hallyburton <jhally@gmail.com>
2019-04-13 22:32:52 -0400

    pull implementation of deck from library

--- algorithms1/queues/Deque.java
+++ algorithms1/queues/Deque.java
@@ -1,19 +1,29 @@
+import edu.princeton.cs.algs4.*;
+
 import java.util.Iterator;
 import java.util.NoSuchElementException;
 
 public class Deque<Item> implements Iterable<Item> {
 
-    private Item[] deque;
-    private int size = 0;
+    private Node<Item> first;    // beginning of queue
+    private Node<Item> last;     // end of queue
+    private int size;
+
+    private static class Node<Item> {
+        private Item item;
+        private Node<Item> next;
+    }
 
     // construct an empty deque
     public Deque() {
-        deque = (Item[]) new Object[2];
+        first = null;
+        last  = null;
+        size  = 0;
     }
 
     // is the deque empty?
     public boolean isEmpty() {
-        return size == 0;
+        return first == null;
     }
 
     // return the number of items on the deque
@@ -23,59 +33,80 @@ public class Deque<Item> implements Iterable<Item> {
 
     // add the item to the front
     public void addFirst(Item item) {
-        if (item.equals("null")) {
-            throw new IllegalArgumentException("item to add cannot be null.");
-        }
+        if (item.equals("null")) throw new IllegalArgumentException("item to add cannot be null.");
+        Node<Item> oldlast = last;
+        last = new Node<Item>();
+        last.item = item;
+        last.next = null;
+        if (isEmpty()) first = last;
+        else oldlast.next = last;
+        size++;
     }
 
     // add the item to the end
     public void addLast(Item item) {
-        if (item.equals("null")) {
-            throw new IllegalArgumentException("item to add cannot be null.");
-        }
+        if (item.equals("null")) throw new IllegalArgumentException("item to add cannot be null.");
+        Node<Item> oldlast = last;
+        last = new Node<Item>();
+        last.item = item;
+        last.next = null;
+        if (isEmpty()) first = last;
+        else oldlast.next = last;
+        size++;
     }
 
     // remove and return the item from the front
-    public int removeFirst() {
-        return 0;
-    }
+//    public Item removeFirst() {
+  //      if (isEmpty()) throw new NoSuchElementException("deque is empty");
+    //    return 0;
+    //}
 
     // remove and return the item from the end
-    public int removeLast() {
-        return 0;
-    }
+    //public Item removeLast() {
+    //    if (isEmpty()) throw new NoSuchElementException("deque is empty");
+    //    return 0;
+    //}
 
     // return an iterator over items in order from front to end
     public Iterator<Item> iterator() {
-        return new ArrayIterator();
+        return new ArrayIterator(first);
     }
 
     // unit testing (optional)
     public static void main(String[] args) {
-        Deque<Integer> test = new Deque<Integer>();
-        Iterator<Integer> test2 = test.iterator();
-        System.out.println(test2);
+        Deque<Integer> queue = new Deque<Integer>();
+        queue.addLast(1);
+        queue.addLast(2);
+        queue.addLast(3);
+        queue.addLast(4);
+        System.out.println(queue.toString());
+        System.out.println(queue.size() + " left on queue");
+    }
+
+    public String toString() {
+        StringBuilder s = new StringBuilder();
+        for (Item item : this) {
+            s.append(item);
+            s.append(' ');
+        }
+        return s.toString();
     }
 
     private class ArrayIterator implements Iterator<Item> {
+        private Node<Item> current;
 
-        private int i;
-
-        public ArrayIterator() {
-            i = size + 1;
+        public ArrayIterator(Node<Item> first) {
+            current = first;
         }
 
-        public boolean hasNext() {
-            return i <= size;
-        }
-
-        public void remove() {
-            throw new UnsupportedOperationException("We don't implement remove");
-        }
+        public boolean hasNext() { return current != null; }
+        public void remove() { throw new UnsupportedOperationException("We don't implement remove"); }
 
         public Item next() {
             if (!hasNext()) throw new NoSuchElementException();
-            return deque[i++];
+            Item item = current.item;
+            current = current.next;
+            return item;
         }
 
     }
